00fdf6fa75ef637b89da9ce7559ad3b31f4c6fa8
Jhonny Cunha <jhonnycunha7@gmail.com>
2025-11-05 20:26:46 -0300

    refact: correções

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.algorithms.second_chance import SecondChance
 from src.core import Access
 from src.algorithms.fifo import Fifo
+from src.algorithms.clock import Clock
 
 def make_sample_trace():
     pages = [1, 2, 3, 2, 4, 1, 5, 2, 1, 2, 3, 4, 5]
@@ -11,10 +12,12 @@ def main():
     trace = make_sample_trace()
     frames_list = [2, 3, 4]
 
-    algos = [Fifo(), SecondChance()]
+    algos = [Fifo(), SecondChance(), Clock()]
     for algo in algos:
         br = algo.benchmark(trace, frames_list)
         algo.plot(f"{algo.name}.png")
+
+
 
 if __name__ == "__main__":
     main()
